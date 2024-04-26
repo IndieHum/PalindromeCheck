cc491fd45519393397851be32d55fd4d7a18340e
IndieHum <moein.fereyduni@gmail.com>
2024-04-26 16:30:26 +0430

    JS is done and logic is fine

---
+++ app.js
@@ -0,0 +1,25 @@
+const CheckButton = document.getElementById("check-btn");
+const EntryInput = document.getElementById("entry");
+const Panel = document.getElementById("panel");
+const RedPanel = document.getElementById("no");
+const BluePanel = document.getElementById("yes");
+
+function ReveseAndPlindromeHandler() {
+  // reset color of palindrome checks
+  BluePanel.style.backgroundColor = "white";
+  RedPanel.style.backgroundColor = "white";
+
+  //making the entry word reverse
+  const EntryInputValue = EntryInput.value;
+  const Revesed = EntryInputValue.split("").reverse().join("").toLowerCase();
+  Panel.innerText = Revesed;
+
+  // check is word palindrome or not
+  if (Revesed == EntryInputValue) {
+    BluePanel.style.backgroundColor = "blue";
+  } else {
+    RedPanel.style.backgroundColor = "red";
+  }
+}
+
+CheckButton.addEventListener("click", ReveseAndPlindromeHandler);
