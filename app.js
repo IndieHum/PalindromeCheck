b9f59062cf84b8ac960bd2ef508d96ddb5cd1b86
IndieHum <moein.fereyduni@gmail.com>
2024-04-27 00:03:47 +0430

    set limit and making some bug of input

--- app.js
+++ app.js
@@ -3,24 +3,35 @@ const EntryInput = document.getElementById("entry");
 const Panel = document.getElementById("panel");
 const RedPanel = document.getElementById("no");
 const BluePanel = document.getElementById("yes");
+const Message = document.getElementById("message");
 
-/// empty entry | one vowel | Limit word ///
+function ShowMessage() {
+  alert("WRONG INPUT");
+}
 
 function ReveseAndPlindromeHandler() {
   // reset color of palindrome checks
   BluePanel.style.backgroundColor = "white";
   RedPanel.style.backgroundColor = "white";
 
-  //making the entry word reverse
   const EntryInputValue = EntryInput.value;
-  const Revesed = EntryInputValue.split("").reverse().join("").toLowerCase();
-  Panel.innerText = Revesed;
-
-  // check is word palindrome or not
-  if (Revesed == EntryInputValue) {
-    BluePanel.style.backgroundColor = "blue";
+  if (!EntryInputValue || EntryInputValue.length < 2) {
+    ShowMessage();
   } else {
-    RedPanel.style.backgroundColor = "red";
+    //making the entry word reverse
+    const Revesed = EntryInputValue.split("")
+      .reverse()
+      .join("")
+      .trim()
+      .toLowerCase();
+    Panel.innerText = Revesed;
+
+    // check is word palindrome or not
+    if (Revesed == EntryInputValue) {
+      BluePanel.style.backgroundColor = "blue";
+    } else {
+      RedPanel.style.backgroundColor = "red";
+    }
   }
 }
 
